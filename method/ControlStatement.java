c087dc43d6733be6930fc48b84d7a33c93d52a93
13020180073 <61999365+13020180073@users.noreply.github.com>
2020-03-29 13:09:52 +0700

    Add files via upload

---
+++ method/ControlStatement.java
@@ -0,0 +1,20 @@
+public class ControlStatement{
+  public static void main (String[] args){
+    int nilai_MTK = 80;
+      
+    if(nilai_MTK < 70){
+      /*
+       * Jika nilai/value pada variable nilai_MTK lebih dari 70
+       * Maka program akan menjalankan statement ini
+       */   
+      System.out.println("Selamat kamu lulus ujian Matematika");
+    }else {
+      /*
+       * Dan Jika nilai/value pada variable nilai_MTK kurang dari 70
+       * Maka program akan menjalankan statement ini
+       */  
+      System.out.println("Mohon maaf kamu tidak lulus");
+    }
+  }
+}
+
